105f4448a51a85589257a7d3a22c7cf3d4aa105b
Your Name <you@example.com>
2025-12-03 17:30:58 +0100

    new classes

--- Elements.hpp
+++ Elements.hpp
@@ -7,43 +7,10 @@
 
 #include <complex>
 
+#define E_LIM 1e-14
+#define F_LIM 1e-12
+
 class Element{
-
-public:
-
-    static unsigned int r_counter;
-    static unsigned int c_counter;
-    static unsigned int l_counter;
-    static unsigned int v_counter;
-
-    char whoAmI(void) const {return this->m_type;}
-
-    unsigned int getId(void) const {return this->m_id;}
-
-    virtual std::complex<double> getAdmitance(double freqency) const = 0;
-
-    void getEndpoints(int &pnode, int &nnode) const {
-        pnode = this->m_endpoints[0];
-        nnode = this->m_endpoints[1];
-    }
-
-    bool isNeighbour(const Element& other, int node) const {
-        if(node != 0){
-           return   (this->m_endpoints[0] == node || this->m_endpoints[1] == node) && 
-                    (other.m_endpoints[0] == node || other.m_endpoints[1] == node);
-        }
-        else{
-            std::cout << "Common node cannot be GND!" << std::endl;
-            return false;
-        }
-    }
-    
-    bool isNeighbour(const Element& other) const { // method overload
-        return  (this->m_endpoints[0] == other.m_endpoints[0] && this->m_endpoints[0] != 0) ||
-                (this->m_endpoints[1] == other.m_endpoints[1] && this->m_endpoints[1] != 0) ||
-                (this->m_endpoints[0] == other.m_endpoints[1] && this->m_endpoints[0] != 0) ||
-                (this->m_endpoints[1] == other.m_endpoints[0] && this->m_endpoints[1] != 0);
-    }
 
 private:
 
@@ -56,13 +23,13 @@ protected:
     std::array<int, 2> m_endpoints;
 
     Element(double val, int pnode, int nnode, unsigned int id, char type) : m_id(id), m_type(type){
-        if(val <= 0){
+        if(val < E_LIM){
             std::string exc = "Value must be positive double!";
             throw exc;
         }
 
         if(pnode < 0 || nnode < 0){
-            std::string exc = "Both + and - nodes must be positive integers or 0";
+            std::string exc = "Both + and - nodes must be positive integers or 0 for GND";
             throw exc;
         }
         this->m_value = val;
@@ -72,14 +39,47 @@ protected:
     double getValue(void) const {return this->m_value;}
 
     void setValue(double val){
-        if(val <= 0){
+        if(val < E_LIM){
             std::string exc = "Value must be positive double!";
             throw exc;
         }
         this->m_value = val;
     }
 
-    virtual ~Element() = default;
+    public:
+
+    static unsigned int r_counter;
+    static unsigned int c_counter;
+    static unsigned int l_counter;
+    static unsigned int v_counter;
+
+    char whoAmI(void) const {return this->m_type;}
+
+    unsigned int getId(void) const {return this->m_id;}
+
+    void getEndpoints(int &pnode, int &nnode) const {
+        pnode = this->m_endpoints[0];
+        nnode = this->m_endpoints[1];
+    }
+
+    bool isNeighbour(const Element& other, int node) const {
+        if(node != 0){
+           return   (this->m_endpoints[0] == node || this->m_endpoints[1] == node) && 
+                    (other.m_endpoints[0] == node || other.m_endpoints[1] == node);
+        }
+        else{
+            std::cout << "Common node cannot be GND for this function, please abort node parameter!" << std::endl;
+            return false;
+        }
+    }
+    
+    bool isNeighbour(const Element& other) const { // method overload
+        return  (this->m_endpoints[0] == other.m_endpoints[0] && this->m_endpoints[0] != 0) ||
+                (this->m_endpoints[1] == other.m_endpoints[1] && this->m_endpoints[1] != 0) ||
+                (this->m_endpoints[0] == other.m_endpoints[1] && this->m_endpoints[0] != 0) ||
+                (this->m_endpoints[1] == other.m_endpoints[0] && this->m_endpoints[1] != 0);
+    }
+
 };
 
 unsigned int Element::r_counter{1};
@@ -87,48 +87,72 @@ unsigned int Element::c_counter{1};
 unsigned int Element::l_counter{1};
 unsigned int Element::v_counter{1};
 
-class Resistance : public Element{
+class Passive : public Element{ //PASSIVE ELEMENTS
+
+protected:
+
+    Passive(double val, int pnode, int nnode, char type) : Element(val, pnode, nnode, this->v_counter++, type){}
+
+    virtual std::complex<double> getAdmitance(double frequency) const = 0;
 
 public:
 
-    Resistance(double r, int pnode, int nnode) : Element(r, pnode, nnode, this->r_counter++, 'R'){}
+    virtual ~Passive() = default;
 
-    std::complex<double> getAdmitance(double freqency) const override {
+};
+
+class Active : public Element{ //ACTIVE ELEMENTS
+
+protected:
+
+    Active(double val, int pnode, int nnode, char type) : Element(val, pnode, nnode, this->v_counter++, type){}
+};
+
+class Resistance : public Passive{
+
+public:
+
+    Resistance(double r, int pnode, int nnode) : Passive(r, pnode, nnode, 'R'){}
+
+    std::complex<double> getAdmitance(double frequency) const override {
         return std::complex<double>((1 / (this->m_value)), 0.0);
     }
 };
 
-class Capacitance : public Element{
+class Capacitance : public Passive{
 
 public:
 
-    Capacitance(double c, int pnode, int nnode) : Element(c, pnode, nnode, this->c_counter++, 'C'){}
+    Capacitance(double c, int pnode, int nnode) : Passive(c, pnode, nnode, 'C'){}
 
-    std::complex<double> getAdmitance(double freqency) const override {
-        return std::complex<double>(0.0, (2 * M_PI * freqency) * (this->m_value));
+    std::complex<double> getAdmitance(double frequency) const override {
+        return std::complex<double>(0.0, (2 * M_PI * frequency) * (this->m_value));
     }
 };
 
-class Inductance : public Element{
+class Inductance : public Passive{
 
 public: 
 
-    Inductance(double l, int pnode, int nnode) : Element(l, pnode, nnode, this->l_counter++, 'L'){}
+    Inductance(double l, int pnode, int nnode) : Passive(l, pnode, nnode, 'L'){}
 
-    std::complex<double> getAdmitance(double freqency) const override {
-        return std::complex<double>(0.0, (-1) / ((2 * M_PI * freqency) * (this->m_value)));
+    std::complex<double> getAdmitance(double frequency) const override {
+        return std::complex<double>(0.0, (-1) / ((2 * M_PI * frequency) * (this->m_value)));
     }
 };
 
-class IndependantV : public Element{
+class IndependantV : public Active{
 
 public: 
 
-    IndependantV(double v, int pnode, int nnode) : Element(v, pnode, nnode, this->v_counter++, 'V'){}
+    IndependantV(double v, int pnode, int nnode) : Active(v, pnode, nnode, 'V'){}
+};
 
-    std::complex<double> getAdmitance(double freqency) const override {
-        return std::complex<double>(0.0, 0.0);
-    }
+class IndependantJ : public Active{
+
+public: 
+
+    IndependantJ(double j, int pnode, int nnode) : Active(j, pnode, nnode, 'J'){}
 };
 
 class Circuit{
@@ -186,32 +210,25 @@ private:
     
 public:
 
-    Circuit(const std::vector<Element*> &elements, double freq) : m_components(elements), m_frequency(freq){
+    Circuit(const std::vector<Element*> &elements, double frequency) : m_components(elements), m_frequency(frequency){
 
-        if(freq < 1e-12){
+        if(frequency < F_LIM){
             std::string exc = "Frequency cannot be zero or negative!"; // dzielenie przez ZERO w funkcjach admitancji (L)!!!
             throw exc;
         }
 
-        this->m_frequency = freq;
+        this->m_frequency = frequency;
 
         buildCircuit(elements);
     }
 
+    double getFrequency(void) const {return this->m_frequency;}
 
-//     // TO MI COS ZLE LICZY NIEEEE BEDE LICZYYYYYL
-//     std::complex<double> same_node_sum(int node){
-//         std::complex<double> sum = 0;
-//         for(int i = 0; i < m_components.size(); i++){
-//             for(int j = 0; j < m_components.size(); j++){
-//                 if(m_components[i]->isNeighbour(*m_components[j], node)){
-//                     sum = m_components[i]->getAdmitance(m_frequency) + m_components[j]->getAdmitance(m_frequency);
-//                 }
-//                 else {
-//                     continue;
-//                 }
-//             }
-//         }
-//         return sum;
-//     }
+    void setFrequency(double frequency){
+        if(frequency < F_LIM){
+            std::string exc = "Frequency cannot be zero or negative!"; // dzielenie przez ZERO w funkcjach admitancji (L)!!!
+            throw exc;
+        }
+        this->m_frequency = frequency;
+    }
 };
